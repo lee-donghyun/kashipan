8fa36cbfa78582c84150677e3b478ce43ed4c58a
Donghyun Lee <leedonghyun022@gmail.com>
2023-08-08 16:17:57 +0900

    feat: 댓글 추가 기능 구현

---
+++ src/components/Comment.tsx
@@ -0,0 +1,27 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  user: {
+    fontWeight: '500',
+    fontSize: 14,
+  },
+  content: {
+    paddingTop: 3,
+    fontSize: 14,
+    opacity: 0.8,
+  },
+});
+export const Comment = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.user}>@유저이름1</Text>
+      <Text style={styles.content}>
+        You can get reference the ScrollView component by setting ref property
+        and using this.refs as described here
+      </Text>
+    </View>
+  );
+};
--- src/stacks/Comments.tsx
+++ src/stacks/Comments.tsx
@@ -1,9 +1,90 @@
-import {Text, View} from 'react-native';
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import {Comment} from '../components/Comment';
+import {IconOutline} from '@ant-design/icons-react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 16,
+    flexDirection: 'row',
+  },
+  headerTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  padding: {
+    paddingTop: 100,
+  },
+  w20: {
+    width: 20,
+  },
+  inputContainer: {
+    position: 'relative',
+    margin: 16,
+    padding: 8,
+    paddingTop: 8,
+    borderRadius: 8,
+    backgroundColor: '#F5F5F5',
+  },
+  submitButton: {
+    position: 'absolute',
+    right: 4,
+    bottom: 4,
+    padding: 8,
+  },
+  submitButtonText: {
+    color: '#3182F6',
+    fontWeight: '600',
+  },
+  input: {padding: 0, paddingTop: 0, marginRight: 32, minHeight: 40},
+});
 
 export const Comments = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>댓글</Text>
+    <View style={styles.background}>
+      <View style={styles.header}>
+        <Pressable
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <IconOutline name="left" size={20} />
+        </Pressable>
+        <Text style={styles.headerTitle}>댓글</Text>
+        <View style={styles.w20} />
+      </View>
+      <ScrollView>
+        <View style={styles.inputContainer}>
+          <TextInput
+            autoFocus
+            multiline
+            placeholder="@유저이름으로 댓글 달기..."
+            style={styles.input}
+          />
+          <Pressable style={styles.submitButton}>
+            <Text style={styles.submitButtonText}>게시</Text>
+          </Pressable>
+        </View>
+        {Array(10)
+          .fill(0)
+          .map((_, key) => (
+            <Comment key={key} />
+          ))}
+        <View style={styles.padding} />
+      </ScrollView>
     </View>
   );
 };
