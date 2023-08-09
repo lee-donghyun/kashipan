61f577dd6dbf095d9389a3b0e8fff73e841e7811
Donghyun Lee <leedonghyun022@gmail.com>
2023-08-09 14:10:31 +0900

    feat: haptic 추가

--- src/screens/Upload.tsx
+++ src/screens/Upload.tsx
@@ -1,13 +1,35 @@
-import {Alert, StyleSheet, View} from 'react-native';
+import {Alert, Pressable, StyleSheet, View} from 'react-native';
 import {Colors} from '../services/constant';
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
 import {useOnce} from '../hooks/useOnce';
 import {useNavigation} from '@react-navigation/native';
+import {useRef, useState} from 'react';
+import {trigger as haptic} from 'react-native-haptic-feedback';
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: Colors.WHITE,
+  },
+  cameraContainer: {
+    flex: 1,
+    backgroundColor: Colors.WHITE,
+    paddingTop: 80,
+  },
+  camera: {
+    aspectRatio: 1,
+  },
+  actionsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  actionButton: {
+    borderRadius: 9999,
+    borderColor: 'black',
+    borderWidth: 8,
+    width: 80,
+    height: 80,
   },
 });
 
@@ -28,8 +50,26 @@ const assertCameraPermission = async () => {
   }
 };
 
+const vibrate = () => haptic('impactMedium');
+
 export const Upload = () => {
   const navigation = useNavigation();
+  const {back: device} = useCameraDevices('wide-angle-camera');
+  const cameraRef = useRef<Camera>(null);
+  const [isRecording, setIsRecording] = useState(false);
+
+  const takePicture = () => {
+    vibrate();
+    cameraRef.current?.takeSnapshot().then(file => console.log(file));
+  };
+
+  const takeVideo = () => {
+    vibrate();
+    cameraRef.current?.startRecording({
+      onRecordingError: () => {},
+      onRecordingFinished: console.log,
+    });
+  };
 
   useOnce(() => {
     assertCameraPermission().catch(message => {
@@ -37,14 +77,38 @@ export const Upload = () => {
       navigation.goBack();
     });
   });
-  const {back: device, ...res} = useCameraDevices();
-  console.log({device, res}, 'ㅠㅠㅠ');
 
   if (device == null) {
     return <View style={styles.background} />;
   }
   return (
-    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    <View style={styles.cameraContainer}>
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        device={device}
+        isActive={true}
+        photo
+      />
+      <View style={styles.actionsContainer}>
+        <Pressable
+          style={({pressed}) => [
+            styles.actionButton,
+            pressed && {opacity: 0.7},
+          ]}
+          onPress={takePicture}
+          onLongPress={() => {
+            setIsRecording(true);
+            takeVideo();
+          }}
+          onPressOut={() => {
+            isRecording && setIsRecording(false);
+            vibrate();
+            cameraRef.current?.stopRecording();
+          }}
+        />
+      </View>
+    </View>
   );
 };
 Upload.name = 'Upload';
