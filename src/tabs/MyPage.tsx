2064042b431e6132201a0e09a47e1346d75a8b41
Donghyun Lee <leedonghyun022@gmail.com>
2023-08-16 11:55:14 +0900

    feat: 마이페이지 ui 구현

--- src/tabs/MyPage.tsx
+++ src/tabs/MyPage.tsx
@@ -1,18 +1,69 @@
-import {ScrollView, StyleSheet, Text} from 'react-native';
-import {Colors} from '../services/constant';
+import {IconFill, IconOutline} from '@ant-design/icons-react-native';
+import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {trigger as haptic} from 'react-native-haptic-feedback';
+import {Spacer} from '../components/Spacer';
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: Colors.WHITE,
-    padding: 40,
+    backgroundColor: '#FFFFFF',
+  },
+  header: {
+    padding: 16,
+    backgroundColor: '#FFFFFF',
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  container: {
+    padding: 16,
+    flex: 1,
+  },
+  label: {
+    opacity: 0.7,
+    fontSize: 12,
+    paddingTop: 20,
+    paddingBottom: 6,
+  },
+  toggle: {
+    fontSize: 16,
+    paddingVertical: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  red: {
+    color: 'red',
+    opacity: 0.7,
   },
 });
 
 export const MyPage = () => {
+  const onPress = () => {
+    haptic('impactMedium');
+  };
   return (
-    <ScrollView style={styles.background}>
-      <Text>MyPage</Text>
-    </ScrollView>
+    <View style={styles.background}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>@my_user_id</Text>
+      </View>
+      <ScrollView style={styles.container}>
+        <Text style={styles.label}>설정</Text>
+        <Pressable onPress={onPress} style={styles.toggle}>
+          <Text>댓글 푸시 알림</Text>
+          <IconFill name="check-circle" size={24} />
+        </Pressable>
+        <Pressable onPress={onPress} style={styles.toggle}>
+          <Text>좋아요 푸시 알림</Text>
+          <IconOutline name="close-circle" size={24} />
+        </Pressable>
+        <Spacer h={40} />
+        <Text style={styles.label}>계정</Text>
+        <Pressable onPress={onPress} style={styles.toggle}>
+          <Text style={styles.red}>로그아웃</Text>
+        </Pressable>
+      </ScrollView>
+    </View>
   );
 };
