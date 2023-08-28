698e8b29b656425be0f8ff0aa018450b14c40f37
Donghyun Lee <donghyun.lee@ngine.co.kr>
2023-08-28 18:05:44 +0900

    feat: 카카오 관련 env 추가

--- src/env.d.test.ts
+++ src/env.d.test.ts
@@ -1,8 +1,9 @@
-import {JUNHO_API_URL} from '@env';
+import {JUNHO_API_URL, KAKAO_API_URL} from '@env';
 import {describe, expect, test} from '@jest/globals';
 
 describe('@env 테스트', () => {
   test('api base 테스트', () => {
-    expect(JUNHO_API_URL).toBe('http://43.201.97.1:8080');
+    expect(JUNHO_API_URL).toBeDefined();
+    expect(KAKAO_API_URL).toBeDefined();
   });
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
 declare module '@env' {
   export const JUNHO_API_URL: string;
+
+  export const KAKAO_API_URL: string;
 }
